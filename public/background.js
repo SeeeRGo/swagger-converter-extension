184ee064e4b7f5ddd7d1d64dc91e5ea76f5a7e06
Sergey Mizin <seeergo@yahoo.com>
2025-07-04 13:46:21 +0400

    тест мгновенной публикации расширений

--- public/background.js
+++ public/background.js
@@ -1,88 +1,89 @@
-// Store request data
-const requests = {}
-const tabRequests = {}
-const finishedRequests = {}
-const blobs = []
-// Listen for web requests
-// Update request data when completed
-chrome.webRequest.onCompleted.addListener(
-  (details) => {    
-    if (details.type === "xmlhttprequest" && (details.url.endsWith('.yaml') || details.url.endsWith('.json') || details.url.endsWith('.yml')) && !finishedRequests[details.url]) {
-      finishedRequests[details.url] = true
+// // Store request data
+// const requests = {}
+// const tabRequests = {}
+// const finishedRequests = {}
+// const blobs = []
+// // Listen for web requests
+// // Update request data when completed
+// chrome.webRequest.onCompleted.addListener(
+//   (details) => {    
+//     if (details.type === "xmlhttprequest" && (details.url.endsWith('.yaml') || details.url.endsWith('.json') || details.url.endsWith('.yml')) && !finishedRequests[details.url]) {
+//       finishedRequests[details.url] = true
       
-      fetch(details.url)
-        .then(data => data.text())
-        .then(blob => {  
-          if(blob.includes('openapi')) {
-            console.log('blobbing');
-            blobs.push({
-              text: blob,
-              type: details.url.endsWith('.json') ? 'json' : 'yaml'
-            })
-          }   
-        })
-        .catch(err => console.error(err));
-    }
-  },
-  { urls: ["<all_urls>"] },
-)
+//       fetch(details.url)
+//         .then(data => data.text())
+//         .then(blob => {  
+//           if(blob.includes('openapi')) {
+//             console.log('blobbing');
+//             blobs.push({
+//               text: blob,
+//               type: details.url.endsWith('.json') ? 'json' : 'yaml'
+//             })
+//           }   
+//         })
+//         .catch(err => console.error(err));
+//     }
+//   },
+//   { urls: ["<all_urls>"] },
+// )
 
-// Update request data when there's an error
-chrome.webRequest.onErrorOccurred.addListener(
-  (details) => {
-    if (requests[details.requestId]) {
-      requests[details.requestId].status = "error"
-      requests[details.requestId].error = details.error
+// // Update request data when there's an error
+// chrome.webRequest.onErrorOccurred.addListener(
+//   (details) => {
+//     if (requests[details.requestId]) {
+//       requests[details.requestId].status = "error"
+//       requests[details.requestId].error = details.error
 
-      // Log to console
-      console.log(`Request error: ${details.error} ${details.url}`)
-    }
-  },
-  { urls: ["<all_urls>"] },
-)
-// Listen for messages from popup
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {  
-  if (message.action === "getRequests") {
-    // Get active tab ID
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs[0]) {
-        const activeTabId = tabs[0].id
+//       // Log to console
+//       console.log(`Request error: ${details.error} ${details.url}`)
+//     }
+//   },
+//   { urls: ["<all_urls>"] },
+// )
+// // Listen for messages from popup
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {  
+//   if (message.action === "getRequests") {
+//     // Get active tab ID
+//     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//       if (tabs[0]) {
+//         const activeTabId = tabs[0].id
 
-        // Get requests for active tab
-        const activeTabRequestIds = tabRequests[activeTabId] || []
-        const activeTabRequestsData = activeTabRequestIds.map((id) => requests[id]).filter(Boolean)
+//         // Get requests for active tab
+//         const activeTabRequestIds = tabRequests[activeTabId] || []
+//         const activeTabRequestsData = activeTabRequestIds.map((id) => requests[id]).filter(Boolean)
 
-        // Sort by timestamp (newest first)
-        activeTabRequestsData.sort((a, b) => b.timestamp - a.timestamp)
-        console.log('blobs in runtime', blobs);
+//         // Sort by timestamp (newest first)
+//         activeTabRequestsData.sort((a, b) => b.timestamp - a.timestamp)
+//         console.log('blobs in runtime', blobs);
         
-        sendResponse({ requests: blobs })
-      } else {
-        sendResponse({ requests: [] })
-      }
-    })
-    return true // Required for async sendResponse
-  } else if (message.action === "clearRequests") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs[0]) {
-        const activeTabId = tabs[0].id
+//         sendResponse({ requests: blobs })
+//       } else {
+//         sendResponse({ requests: [] })
+//       }
+//     })
+//     return true // Required for async sendResponse
+//   } else if (message.action === "clearRequests") {
+//     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//       if (tabs[0]) {
+//         const activeTabId = tabs[0].id
 
-        // Clear requests for active tab
-        if (tabRequests[activeTabId]) {
-          tabRequests[activeTabId].forEach((id) => {
-            delete requests[id]
-          })
-          tabRequests[activeTabId] = []
-        }
+//         // Clear requests for active tab
+//         if (tabRequests[activeTabId]) {
+//           tabRequests[activeTabId].forEach((id) => {
+//             delete requests[id]
+//           })
+//           tabRequests[activeTabId] = []
+//         }
 
-        sendResponse({ success: true })
-      }
-    })
-    return true
-  } else if (message.action === "parseRequests") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      sendResponse({ requests: blobs })
-    })
-    return true
-  }
-})
+//         sendResponse({ success: true })
+//       }
+//     })
+//     return true
+//   } else if (message.action === "parseRequests") {
+//     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//       sendResponse({ requests: blobs })
+//     })
+//     return true
+//   }
+// })
+console.log('extension started');
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { UploadSwagger } from './components/upload-swagger.js';
-import { ConvertOnline } from './components/convert-online.js'; // @ts-ignore
+// import { ConvertOnline } from './components/convert-online.js'; // @ts-ignore
 import imgUrl from './assets/github-mark.png'
 
 const strings = {
@@ -11,7 +11,7 @@ function App() {
     <div className="w-[350px] flex flex-col gap-1">
       <p className='text-lg'>{strings.title}</p>
       <UploadSwagger />
-      <ConvertOnline />
+      {/* <ConvertOnline /> */}
       <div className='flex flex-row justify-between'>
       <a href="https://pay.cloudtips.ru/p/6b58eceb" target="_blank">Поддержать рублём</a>
       <a className="size-[24px]" href="https://github.com/SeeeRGo/swagger-converter-extension" target="_blank">
